fcdfec7eb0d697d8ad2a0cb89d681ed803ed2b88
Robert Lafferty <frogboythatsme@gmail.com>
2025-08-05 17:58:39 -0700

    Added website sitemap to help with SEO

---
+++ src/generate-sitemap.js
@@ -0,0 +1,25 @@
+const sitemap = require('sitemap');
+const fs = require('fs');
+
+const serviceIds = ['unity', 'unreal'];
+const bonusIds = ['game-vs-game-engine', 'unity-vs-unreal'];
+
+const urls = [
+  { url: '/', changefreq: 'daily', priority: 1 },
+  { url: '/services', changefreq: 'weekly', priority: 0.8 },
+  ...serviceIds.map(id => ({ url: `/services/${id}`, changefreq: 'weekly', priority: 0.7 })),
+  { url: '/bonus', changefreq: 'weekly', priority: 0.8 },
+  ...bonusIds.map(id => ({ url: `/bonus/${id}`, changefreq: 'weekly', priority: 0.7 })),
+  { url: '/faq', changefreq: 'monthly', priority: 0.5 },
+  { url: '/book', changefreq: 'monthly', priority: 0.5 },
+  { url: '/about', changefreq: 'monthly', priority: 0.5 },
+  { url: '/nfc', changefreq: 'monthly', priority: 0.5 },
+  { url: '/privacy-policy', changefreq: 'monthly', priority: 0.5 },
+];
+
+const sitemapInstance = sitemap.createSitemap({
+  hostname: 'https://www.guildofbeginnings.com',
+  urls: urls,
+});
+
+fs.writeFileSync('./public/sitemap.xml', sitemapInstance.toString());
